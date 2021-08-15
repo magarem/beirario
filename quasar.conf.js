955efbc21a3bb9df58744c06cf9e5a44121962a7
marcelo <contato@magaweb.com.br>
2021-08-14 21:33:04 -0300

    pwa2

--- quasar.conf.js
+++ quasar.conf.js
@@ -102,7 +102,7 @@ module.exports = configure(function (ctx) {
 
     // https://v2.quasar.dev/quasar-cli/developing-ssr/configuring-ssr
     ssr: {
-      pwa: false,
+      pwa: true,
 
       // manualStoreHydration: true,
       // manualPostHydrationTrigger: true,
@@ -146,30 +146,30 @@ module.exports = configure(function (ctx) {
         theme_color: '#027be3',
         icons: [
           {
-            src: 'icons/icon-128x128.png',
+            src: 'icons/favicon-128x128.png',
             sizes: '128x128',
             type: 'image/png'
-          },
-          {
-            src: 'icons/icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'icons/icon-256x256.png',
-            sizes: '256x256',
-            type: 'image/png'
-          },
-          {
-            src: 'icons/icon-384x384.png',
-            sizes: '384x384',
-            type: 'image/png'
-          },
-          {
-            src: 'icons/icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
           }
+          // {
+          //   src: 'icons/icon-192x192.png',
+          //   sizes: '192x192',
+          //   type: 'image/png'
+          // },
+          // {
+          //   src: 'icons/icon-256x256.png',
+          //   sizes: '256x256',
+          //   type: 'image/png'
+          // },
+          // {
+          //   src: 'icons/icon-384x384.png',
+          //   sizes: '384x384',
+          //   type: 'image/png'
+          // },
+          // {
+          //   src: 'icons/icon-512x512.png',
+          //   sizes: '512x512',
+          //   type: 'image/png'
+          // }
         ]
       }
     },
